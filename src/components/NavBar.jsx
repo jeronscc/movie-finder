1c3f7174c25df39b42b42f46fa61ee44261876ca
Jeron Cansanay <jeronscc@gmail.com>
2025-05-20 17:10:18 +0800

    updated logo and favicon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,27 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <Link to="/">Movie Finder</Link>
-            </div>
-            <div className="navbar-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/favorites" className="nav-link">Favorites</Link>
-            </div>
-        </nav>
-
-    )
+  return (
+    <nav className="navbar">
+      <div className="brand-container">
+        <Link to="/">
+          <img
+            src="/images/brand.png"
+            alt="Brand Logo"
+            className="brand-logo"
+          />
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="nav-link">
+          Favorites
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
