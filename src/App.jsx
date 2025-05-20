0513a80702fb604df16f0f94947ee2758ab73a34
Jeron Cansanay <jeronscc@gmail.com>
2025-05-20 22:37:06 +0800

    added context provider for favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import './css/App.css'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import NavBar from './components/NavBar'
-import {Routes, Route} from "react-router-dom"
+import "./css/App.css";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <NavBar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="*" element={<div>404 Not Found</div>} />
-          </Routes>
-        </main>
+        <MovieProvider>
+          <NavBar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="*" element={<div>404 Not Found</div>} />
+            </Routes>
+          </main>
+        </MovieProvider>
       </div>
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,30 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites && favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
   return (
-    <div className="favorite-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
+    <div className="favorites-empty">
+      <h2>Your Favorites List is Empty</h2>
+      <p>
+        Browse movies and click the ‪‪❤︎‬ icon to add them here. Start building
+        your watchlist!
+      </p>
     </div>
   );
 }
